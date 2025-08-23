f241f36cf48f30a23ad11e92ec4b0110741ab890
Wojciech Sadowski <wojciechnwd@gmail.com>
2025-08-23 17:34:36 +0200

    Creation of a reusable Products component, creation of an object array storing data that is delivered using .map to use Products with different content and styles. -- This solution was used to reduce the amount of code. A ProductProps type alias was added and exported.

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,6 +1,68 @@
 import projectImg from "../assets/photos/projektowanie-ogrodów-small.jpg";
 import createImg from "../assets/photos/zakladanie-ogrodu.jpg";
+import cuttingImg from "../assets/photos/garden-cutting.png";
+import cleaningImg from "../assets/photos/window-cleaning.png";
+import type { ProductProps } from "./Product";
+import Product from "./Product";
 import styles from "./Offer.module.scss";
+
+const productsData: ProductProps[] = [
+	{
+		bgColor: "firstBgc",
+		img: projectImg,
+		alt: "zdjęcie siedzącego przy stole projektanta, rysującego projekt ogrodu.",
+		title: "Projektowanie ogrodów",
+		services: [
+			"Projekty ogrodów przydomowych",
+			"Projekty ogrodów na dużych powierzchniach",
+			"Kompleksowe projekty zagospodarowania terenów zielonych",
+			"Projekty terenów rekreacyjnych, parków miejskich",
+			"Projekty systemów nawadniania i oświetlenia",
+		],
+	},
+	{
+		bgColor: "secondBgc",
+		img: createImg,
+		extraImgStyle: "img2",
+		alt: "Ogrodnik próbuje zasadzić młodą roślinę w ziemii.",
+		title: "Zakładanie ogrodów",
+		services: [
+			"Kompleksowe zakładanie ogrodów",
+			"Prace ziemne",
+			"Zakładanie trawników",
+			"Nasadzenia roślinne",
+			"Altany, pergole, oczka wodne",
+		],
+	},
+	{
+		bgColor: "firstBgc",
+		img: cuttingImg,
+		extraImgStyle: "img4",
+		alt: "Ogrodnik przycinający żyopłot, w tle sosna a za nią szerokie schody przypominające te znane z szkół publicznych.",
+		title: "Pielęgnacja ogrodów",
+		services: [
+			"Cięcia roślin",
+			"Wertykulacja i areacja",
+			"Nawożenie roślin",
+			"Zabezpieczanie przed insektami, chorobami, opryski",
+			"Koszenie oraz sprzątanie",
+		],
+	},
+	{
+		bgColor: "fourthBgc",
+		img: cleaningImg,
+		extraImgStyle: "img2",
+		alt: "Okno pokryte płynem czyszczącym, myte za pomocą niebieskiej ściągaczki.",
+		title: "Usługi sprzątające",
+		services: [
+			"Dbanie o czystość obejścia",
+			"Sprzątanie klatek schodowych",
+			"Mycie okien - osiedla",
+			"Grabienie liści",
+			"Odśnieżanie zimą",
+		],
+	},
+];
 
 export default function Offer() {
 	return (
@@ -13,49 +75,17 @@ export default function Offer() {
 				<p>Sprawdź nas!</p>
 			</div>
 
-			{/* First offer */}
-			<div className={`${styles.categoryBlock} ${styles.firstBgc}`}>
-				<div className={styles.imgFrame}>
-					<img
-						className={styles.img}
-						src={projectImg}
-						alt="zdjęcie siedzącego przy stole projektanta, rysującego projekt ogrodu."
-					/>
-				</div>
-
-				<div className={styles.contentBox}>
-					<h3 className={styles.contentTitle}>Projektowanie ogrodów</h3>
-					<ul>
-						<li>Projekty ogrodów przydomowych</li>
-						<li>Projekty ogrodów na dużych powierzchniach</li>
-						<li>Kompleksowe projekty zagospodarowania terenów zielonych</li>
-						<li>Projekty terenów rekreacyjnych, parków miejskich</li>
-						<li>Projekty systemów nawadniania i oświetlenia</li>
-					</ul>
-				</div>
-			</div>
-
-			<div className={styles.categoryBlock}>
-				<div className={styles.imgFrame}>
-					<img
-					// do zmiany
-						className={`${styles.img} ${styles.img2}`}
-						src={createImg}
-						alt="Ogrodnik próbuje zasadzić młodą roślinę w ziemii."
-					/>
-				</div>
-
-				<div className={styles.contentBox}>
-					<h3 className={styles.contentTitle}>Zakładanie ogrodów</h3>
-					<ul>
-						<li>Kompleksowe zakładanie ogrodów</li>
-						<li>Prace ziemne</li>
-						<li>Zakładanie trawników</li>
-						<li>Nasadzenia roślinne</li>
-						<li>Altany, pergole, oczka wodne, inne</li>
-					</ul>
-				</div>
-			</div>
+			{productsData.map((product) => (
+				<Product
+					key={product.title}
+					bgColor={product.bgColor}
+					img={product.img}
+					extraImgStyle={product.extraImgStyle}
+					alt={product.alt}
+					title={product.title}
+					services={product.services}
+				/>
+			))}
 		</section>
 	);
 }
---
+++ src/components/Product.tsx
@@ -0,0 +1,41 @@
+import styles from "./Product.module.scss";
+
+export type ProductProps = {
+	bgColor: "firstBgc" | "secondBgc" | "fourthBgc";
+	img: string;
+	extraImgStyle?: "img2" | "img4";
+	alt: string;
+	title: string;
+	services: string[];
+};
+
+export default function Product({
+	bgColor,
+	img,
+	alt,
+	title,
+	services,
+	extraImgStyle,
+}: ProductProps) {
+	return (
+		<div className={`${styles.categoryBlock} ${styles[bgColor]}`}>
+			<div className={styles.imgFrame}>
+				<img
+					className={`${styles.img} ${
+						extraImgStyle ? styles[extraImgStyle] : ""
+					}`}
+					src={img}
+					alt={alt}
+				/>
+			</div>
+			<div className={styles.contentBox}>
+				<h3 className={styles.contentTitle}>{title}</h3>
+				<ul>
+					{services.map((item) => (
+						<li key={item}>{item}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
+}
