5ff3cb1506ff30fe2def77bf17775d8267a5ffb5
Wojciech Sadowski <wojciechnwd@gmail.com>
2025-09-08 22:11:42 +0200

    The Footer component was created and adjusted to mobile width, as well as 768, 992, and 1200px.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Projects from "./components/Projects";
 import Reviews from "./components/Reviews";
 import SectionButton from "./components/SectionButton";
 import "./App.scss";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 				<Projects />
 				<Reviews />
 			</main>
+			<Footer />
 		</>
 	);
 }
